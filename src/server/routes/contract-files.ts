a1cff55c4f64f003057c9a669c4664509092f233
wendy hu <wendyhu@wendys-Laptop.local>
2025-09-07 13:04:17 -0700

    Fix file upload: add support for markdown and text files

--- src/server/routes/contract-files.ts
+++ src/server/routes/contract-files.ts
@@ -23,6 +23,7 @@ const upload = multer({
       'application/msword',
       'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
       'text/plain',
+      'text/markdown',
       'application/vnd.ms-excel',
       'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
     ];
